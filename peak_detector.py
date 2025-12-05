3600555ea7a306fa09c7c3b08a9611ac08177743
David Bozhanov <davnov015@gmail.com>
2025-12-05 01:33:34 -0600

    Handle flat data in peak detector, remove clump detection, lookahead

--- peak_detector.py
+++ peak_detector.py
@@ -1,8 +1,10 @@
+import math
+
 import numpy as np
 
+delta_index = 100
 
 def frequency_to_index_map(frequency: np.typing.NDArray, index: int):
-    delta_index = 100
     assert frequency.size > index
     assert frequency.size > index and frequency.size > delta_index
     if frequency.size - delta_index <= index:
@@ -13,6 +15,8 @@ def frequency_to_index_map(frequency: np.typing.NDArray, index: int):
 
 def window_size_function(frequency: np.typing.NDArray, index: int, frequency_window_size: int) -> int:
     dIdF = frequency_to_index_map(frequency, index)
+    if math.isinf(dIdF):
+        return delta_index
     return int(frequency_window_size * dIdF)
 
 def find_peaks(data: np.typing.NDArray, frequency: np.typing.NDArray, frequency_window_size: int, frequency_window_offset: int = 0):
@@ -20,6 +24,7 @@ def find_peaks(data: np.typing.NDArray, frequency: np.typing.NDArray, frequency_
     next_window_size = window_size_function(frequency, 0, frequency_window_size)
     next_window_offset = window_size_function(frequency, 0, frequency_window_offset)
     data_index = 0
+    delta_index_cycle = False
     while data_index + next_window_size <= data.size:
         window_start = data_index + next_window_offset
         window_end = window_start + next_window_size
@@ -27,19 +32,30 @@ def find_peaks(data: np.typing.NDArray, frequency: np.typing.NDArray, frequency_
         if data_index >= data.size:
             break
         next_window_size = window_size_function(frequency, data_index, frequency_window_size)
+        if next_window_size == delta_index:
+            data_index += delta_index
+            delta_index_cycle = True
+            continue
         next_window_offset = window_size_function(frequency, data_index, frequency_window_offset)
+        if delta_index_cycle:
+            delta_index_cycle = False
+            continue
         window = data[window_start:window_end]
         max_index = np.argmax(window)
         max_value = window[max_index]
         window_edge_offset = 3
         if max_index < window_edge_offset or max_index > frequency_window_size - window_edge_offset:
             continue
+        if max_value < 0.05:
+            continue
         # Lookahead check
         failure = False
-        for i in range(next_window_size // 10):
-            if max_value < data[window_end + i]:
-                failure = True
-                break
+        next_window_range = next_window_size // 5
+        if data_index + next_window_range <= data.size:
+            for i in range(next_window_range):
+                if max_value < data[window_end + i]:
+                    failure = True
+                    break
         if not failure:
             max_pos = window_start + max_index
             peaks.append(max_pos)
@@ -49,13 +65,9 @@ def find_peaks(data: np.typing.NDArray, frequency: np.typing.NDArray, frequency_
         for i in range(1, 20):
             fresh_peaks = find_peaks(data, frequency, frequency_window_size, i * frequency_window_size // 10)
             new_peaks = np.union1d(new_peaks, fresh_peaks)
-        # peaks = np.unique(new_peaks)
         peaks = np.union1d(peaks, new_peaks)
-        bins = np.linspace(0, data.size - 1, data.size // 100)
-        frequencies, _ = np.histogram(peaks, bins=bins, density=False)
-        assert np.where(frequencies > 1)[0].size == 0
+        # bins = np.linspace(0, data.size - 1, data.size // 100)
+        # frequencies, _ = np.histogram(peaks, bins=bins, density=False)
+        # assert np.where(frequencies > 1)[0].size == 0, "Clumping of peak values detected!"
         return peaks
-    return np.array(peaks)
-
-# def cleanup_redundant_peaks(peaks: np.typing.NDArray, frequency: np.typing.NDArray, frequency_window_size: int):
-#     return peaks
+    return np.array(peaks, dtype=np.int64)
